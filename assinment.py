ce8da856a1a4748c327389bf0b59e39f1fd25b3f
Ali Mustapha <alimustapha0909@gmail.com>
2025-08-07 07:57:25 +0100

    Add files via upload

---
+++ assinment.py
@@ -0,0 +1,23 @@
+print("1 - add")
+print("2 - subtract")
+print("3 - multiply")
+print("4 - divide")
+option = int(input("choose an operation"))
+
+if(option in [1,2,3,4]):
+    num1 = int(input("Enter first number"))
+    num2 = int(input("Enter second number"))
+
+    if(option == 1):
+        result = num1 + num2
+    elif(option == 2):
+        result = num1 - num2
+    elif(option == 3):
+        result = num1 * num2
+    elif(option == 4):
+        result = num1 // num2
+
+else:
+    print("Invalid operation entered")
+
+print("The result of the operation is" .format(result))
